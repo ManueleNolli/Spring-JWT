411ed12830d0057838f79a9e6c450e6c33954517
Manuele Nolli <manuele.nolli@hotmail.it>
2022-11-14 09:31:07 +0100

    Server Spring Boot init

---
+++ jwtSecurityServer/src/main/java/nolli/backend/jwtsecurityserver/JwtSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package nolli.backend.jwtsecurityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityServerApplication.class, args);
+    }
+
+}
---
+++ jwtSecurityServer/src/test/java/nolli/backend/jwtsecurityserver/JwtSecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nolli.backend.jwtsecurityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

d156902832cf6ce60ff8a9f1017d097e4dcc611d
Manuele Nolli <manuele.nolli@hotmail.it>
2022-11-18 10:41:15 +0100

    created client spring server

---
+++ jwtSecurityClient/src/main/java/security/frontend/jwtsecurityclient/JwtSecurityClientApplication.java
@@ -0,0 +1,13 @@
+package security.frontend.jwtsecurityclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityClientApplication.class, args);
+    }
+
+}

637391de89a1e9db3332b7d09e50b190aadd3252
Davis Fusco <davis.fusco.20@gmail.com>
2022-11-25 11:48:52 +0100

    feat: client controller riquest

--- jwtSecurityClient/src/main/java/security/frontend/jwtsecurityclient/ClientController.java
+++ jwtSecurityClient/src/main/java/security/frontend/jwtsecurityclient/ClientController.java
@@ -1,10 +1,28 @@
 package security.frontend.jwtsecurityclient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+import security.frontend.jwtsecurityclient.Model.User;
 
 @Controller
 public class ClientController {
+    private final RestTemplate restTemplate;
+
+    private static final String REGISTRATION_URL = "http://localhost:8080/register";
+    private static final String AUTHENTICATION_URL = "http://localhost:8080/authenticate";
+    private static final String HELLO_URL = "http://localhost:8080/helloadmin";
+    private static final String REFRESH_TOKEN = "http://localhost:8080/refreshtoken";
+
+    public ClientController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/")
     public String login() {
         return "login";
@@ -16,7 +34,43 @@ public class ClientController {
     }
 
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(Model model) {
+        model.addAttribute("user", new User());
         return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(User user) throws JsonProcessingException {
+        // convert the user registration object to JSON
+        String registrationBody = getBody(user);
+
+        // create headers specifying that it is JSON request
+        HttpHeaders registrationHeaders = getHeaders();
+        HttpEntity<String> registrationEntity = new HttpEntity<String>(registrationBody, registrationHeaders);
+
+        try {
+            // Register User
+            ResponseEntity<String> registrationResponse = restTemplate.exchange(REGISTRATION_URL, HttpMethod.POST,
+                    registrationEntity, String.class);
+
+            if (registrationResponse.getStatusCode().equals(HttpStatus.OK)) {
+                return "redirect:/";
+            } else {
+                return "redirect:/error";
+            }
+        } catch (Exception e) {
+            return "redirect:/error";
+        }
+    }
+
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        return headers;
+    }
+
+    private String getBody(final User user) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(user);
     }
 }

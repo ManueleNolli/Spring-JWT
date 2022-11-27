5db3b56a45cc9dbdbd75fc0211129dafa51725ed
Manuele Nolli <manuele.nolli@hotmail.it>
2022-11-27 11:22:11 +0100

    save token in cookies

--- jwtSecurityClient/src/main/java/security/frontend/jwtsecurityclient/ClientController.java
+++ jwtSecurityClient/src/main/java/security/frontend/jwtsecurityclient/ClientController.java
@@ -11,15 +11,22 @@ import org.springframework.web.client.RestTemplate;
 import security.frontend.jwtsecurityclient.Model.ResponseToken;
 import security.frontend.jwtsecurityclient.Model.User;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+
 @Controller
 public class ClientController {
     private final RestTemplate restTemplate;
 
     private static final String REGISTRATION_URL = "http://localhost:8080/register";
     private static final String AUTHENTICATION_URL = "http://localhost:8080/authenticate";
-    private static final String HOME_URL = "redirect:/home";
-    private static final String ERROR_URL = "redirect:/error";
     private static final String REFRESH_TOKEN = "http://localhost:8080/refreshtoken";
+
+    private static final String HOME_URL = "redirect:/home";
+    private static final String LOGIN_URL = "redirect:/";
+    private static final String ERROR_URL = "redirect:/error";
     private String token;
 
     public ClientController(RestTemplate restTemplate) {
@@ -33,7 +40,12 @@ public class ClientController {
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(HttpServletRequest request) {
+        getToken(request);
+        if (token == null) {
+            return ERROR_URL;
+        }
+        System.out.println("Token: " + token);
         return "home";
     }
 
@@ -54,7 +66,12 @@ public class ClientController {
     }
 
     @GetMapping("/adminPage")
-    public String adminPage() {
+    public String adminPage(HttpServletRequest request) {
+        getToken(request);
+        if (token == null) {
+            return ERROR_URL;
+        }
+        System.out.println("Token: " + token);
         return "adminPage";
     }
 
@@ -66,7 +83,7 @@ public class ClientController {
 
         // create headers specifying that it is JSON request
         HttpHeaders registrationHeaders = getHeaders();
-        HttpEntity<String> registrationEntity = new HttpEntity<String>(registrationBody, registrationHeaders);
+        HttpEntity<String> registrationEntity = new HttpEntity<>(registrationBody, registrationHeaders);
 
         try {
             // Register User
@@ -74,7 +91,7 @@ public class ClientController {
                     registrationEntity, String.class);
 
             if (registrationResponse.getStatusCode().equals(HttpStatus.OK)) {
-                response = HOME_URL;
+                response = LOGIN_URL;
             } else {
                 response = ERROR_URL;
             }
@@ -85,42 +102,49 @@ public class ClientController {
     }
 
     @PostMapping("/login")
-    public String login(User user) throws JsonProcessingException {
-        String response = null;
+    public String handleLogin(User user, HttpServletResponse responseHttp) throws JsonProcessingException {
+        String response;
         // convert the user registration object to JSON
-        String authenticationBody = getBody(user);
+        String loginBody = getBody(user);
 
         // create headers specifying that it is JSON request
-        HttpHeaders authenticationHeaders = getHeaders();
-        HttpEntity<String> authenticationEntity = new HttpEntity<String>(authenticationBody, authenticationHeaders);
-
+        HttpHeaders loginHeaders = getHeaders();
+        HttpEntity<String> loginEntity = new HttpEntity<>(loginBody, loginHeaders);
         try {
-            // Authenticate User and get JWT
-            ResponseEntity<ResponseToken> authenticationResponse = restTemplate.exchange(AUTHENTICATION_URL,
-                    HttpMethod.POST, authenticationEntity, ResponseToken.class);
+            // Login User
+            ResponseEntity<ResponseToken> loginResponse = restTemplate.exchange(AUTHENTICATION_URL, HttpMethod.POST,
+                    loginEntity, ResponseToken.class);
 
-            // if the authentication is successful
-            if (authenticationResponse.getStatusCode().equals(HttpStatus.OK)) {
-                token = "Bearer " + authenticationResponse.getBody().getToken();
+            token = Objects.requireNonNull(loginResponse.getBody()).getToken();
+            if (loginResponse.getStatusCode().equals(HttpStatus.OK)) {
+                Cookie jwtTokenCookie = new Cookie("Authorization",token );
+                jwtTokenCookie.setHttpOnly(true);
+                jwtTokenCookie.setSecure(true);
+                jwtTokenCookie.setPath("/");
+                jwtTokenCookie.setMaxAge(3600);
+                responseHttp.addCookie(jwtTokenCookie);
                 response = HOME_URL;
             } else {
                 response = ERROR_URL;
             }
         } catch (Exception e) {
-            // check if exception is due to ExpiredJwtException
-            if (e.getMessage().contains("io.jsonwebtoken.ExpiredJwtException")) {
-                // Refresh Token
-                refreshToken();
-                // try again with refresh token
-                response = HOME_URL;
-            }else {
-                response = ERROR_URL;
-            }
+            response = ERROR_URL;
         }
         return response;
     }
 
-    private String getData() {
+        private String getToken(HttpServletRequest request) {
+            if (request.getCookies() != null) {
+                for (Cookie cookie : request.getCookies()) {
+                    if (cookie.getName().equals("Authorization")) {
+                        token = cookie.getValue();
+                    }
+                }
+            }
+            return token;
+        }
+
+/*    private String getData() {
         String response = null;
 
         HttpHeaders headers = getHeaders();
@@ -133,18 +157,18 @@ public class ClientController {
             response = helloResponse.getBody();
         }
         return response;
-    }
+    }*/
 
     private void refreshToken() {
         HttpHeaders headers = getHeaders();
         headers.set("Authorization", token);
         headers.set("isRefreshToken", "true");
-        HttpEntity<String> jwtEntity = new HttpEntity<String>(headers);
+        HttpEntity<String> jwtEntity = new HttpEntity<>(headers);
         // Use Token to get Response
         ResponseEntity<ResponseToken> refreshTokenResponse = restTemplate.exchange(REFRESH_TOKEN, HttpMethod.GET, jwtEntity,
                 ResponseToken.class);
         if (refreshTokenResponse.getStatusCode().equals(HttpStatus.OK)) {
-            token = "Bearer " +refreshTokenResponse.getBody().getToken();
+            token = "Bearer " + Objects.requireNonNull(refreshTokenResponse.getBody()).getToken();
         }
     }
 
